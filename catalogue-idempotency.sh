be9e0651a16907f77180ddbe443f7246ddc7edc0
gsurendhar <surendharg123@gmail.com>
2025-05-24 16:44:05 +0530

    catalogue idempotency setup file is added

---
+++ catalogue-idempotency.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+source ./main_common.sh
+app_name=catalogue
+
+CHECK_ROOT
+
+APP_SETUP
+
+NODEJS_SETUP"
+
+SYSTEMD_SETUP
+
+cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongodb.repo 
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing Mongodb Client"
+
+STATUS=$(mongosh --host mongo.daws84s.site --eval ' db.getMongo().getDBNames.indexof(""catalogue")') &>>$LOG_FILE
+if [ $STATUS -lt 0 ]
+then 
+    mongosh --host mongodb.daws84s.site < /app/db/master-data.js
+    VALIDATE $? "Loading the Data into MongoDB"
+else    
+    echo -e "Data is already loaded ....$Y SKIPPING $N "
+fi
+
+PRINT_TIME
+
