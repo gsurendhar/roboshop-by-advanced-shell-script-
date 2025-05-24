208ba27b48abc77fcd84fcfdc99c3495e91e8caa
gsurendhar <surendharg123@gmail.com>
2025-05-24 16:37:28 +0530

    shipping app setup file is added

---
+++ shipping.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+source ./main_common.sh
+app_name=shipping
+
+CHECK_ROOT
+
+APP_SETUP
+
+MAVEN_SETUP
+
+SYSTEMD_SETUP
+
+echo "Please enter MySql Root Password"  | tee -a $LOG_FILE
+read -s MYSQL_ROOT_PASSWORD
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing MySql Client"
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql  &>>$LOG_FILE
+VALIDATE $? "Loading SCHEMAS to MySQL"
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>$LOG_FILE
+VALIDATE $? "Loading APP-USER DATA to MySQL"
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+VALIDATE $? "Loading MASTER_DATA to MySQL"
+
+systemctl restart shipping  &>>$LOG_FILE
+VALIDATE $? "Restarting Shipping Services" 
+
+PRINT_TIME
