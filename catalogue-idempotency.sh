252828a828f079192e77a193f3be69f42ec80f08
gsurendhar <surendharg123@gmail.com>
2025-05-31 16:15:04 +0530

    all file updated

--- catalogue-idempotency.sh
+++ catalogue-idempotency.sh
@@ -14,10 +14,11 @@ cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongodb.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing Mongodb Client"
 
-STATUS=$(mongosh --host mongo.daws84s.site --eval ' db.getMongo().getDBNames.indexof(""catalogue")') &>>$LOG_FILE
+STATUS=$(mongosh --host mongo.gonela.site --eval ' db.getMongo().getDBNames.indexof("catalogue")') &>>$LOG_FILE
+
 if [ $STATUS -lt 0 ]
 then 
-    mongosh --host mongodb.daws84s.site < /app/db/master-data.js
+    mongosh --host mongodb.gonela.site < /app/db/master-data.js
     VALIDATE $? "Loading the Data into MongoDB"
 else    
     echo -e "Data is already loaded ....$Y SKIPPING $N "
